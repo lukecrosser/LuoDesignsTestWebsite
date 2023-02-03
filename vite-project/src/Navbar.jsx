525657afa2b1311e813008298b17ab4758c4cf82
lukecrosser <lukecrosser@hotmail.com>
2023-02-03 12:03:13 -0700

    trying to add a second page

--- vite-project/src/Navbar.jsx
+++ vite-project/src/Navbar.jsx
@@ -1,4 +1,5 @@
 import './index.css';
+import { BrowserRouter } from 'react-router-dom'
 
 function Navbar() {
     return (
@@ -15,7 +16,7 @@ function Navbar() {
                             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
                         </a>
                         <ul className="p-2 bg-base-100">
-                            <li><a href="">Submenu 1</a></li>
+                            <li><a href="about\main.jsx">Submenu 1</a></li>
                             <li><a>Submenu 2</a></li>
                         </ul>
                     </li>
